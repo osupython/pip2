30e2a05c989c61909268a968db3155f8f0eb6a1b
Ryan Baker <github@rlbaker.net>
2012-01-10 11:18:52 -0800

    Refactored commandline parser and added editable option.

---
+++ pip2/parser.py
@@ -0,0 +1,24 @@
+import argparse
+import commands
+
+def create_parser():
+    parser = argparse.ArgumentParser(prog='pip2')
+    subparsers = parser.add_subparsers()
+
+    parser_install = subparsers.add_parser('install')
+    parser_install.add_argument('package_name')
+    parser_install.add_argument('-e', '--editable', action='store_true')
+    parser_install.set_defaults(func=commands.install)
+
+    parser_uninstall = subparsers.add_parser('uninstall')
+    parser_uninstall.add_argument('package_name')
+    parser_uninstall.set_defaults(func=commands.uninstall)
+
+    parser_search = subparsers.add_parser('search')
+    parser_search.add_argument('package_name')
+    parser_search.set_defaults(func=commands.search)
+
+    parser_freeze = subparsers.add_parser('freeze')
+    parser_freeze.set_defaults(func=commands.freeze)
+
+    return parser
