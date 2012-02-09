8e14de838a13373e00e4e2607ede9e0e7ada09d7
Ryan Baker <github@rlbaker.net>
2012-02-09 15:54:06 -0800

    Replaced patching on each test with patching of test class to reduce code duplication

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,10 +1,10 @@
 import mock
 import pip2.commands.install
+import distutils2.install
 
-
+@mock.patch.object(distutils2.install, 'install')
 class TestInstallAPI():
 
-    @mock.patch.object(pip2.commands.install, 'func')
     def test_install_single_package(self, mock_func):
         mock_func.return_value = True
         expected = ['test_package1']
@@ -13,7 +13,6 @@ class TestInstallAPI():
         assert result['installed'] == expected
         assert result['failed'] == []
 
-    @mock.patch.object(pip2.commands.install, 'func')
     def test_install_multiple_packages(self, mock_func):
         mock_func.return_value = True
         expected = ['test_package1', 'test_package2', 'test_package3']
@@ -22,7 +21,6 @@ class TestInstallAPI():
         assert result['installed'] == expected
         assert result['failed'] == []
 
-    @mock.patch.object(pip2.commands.install, 'func')
     def test_install_fail_single_package(self, mock_func):
         mock_func.return_value = False
         expected = ['failing_package']
@@ -31,25 +29,27 @@ class TestInstallAPI():
         assert result['installed'] == []
         assert result['failed'] == expected
 
-    @mock.patch.object(pip2.commands.install, 'func')
     def test_install_fail_multiple_packages(self, mock_func):
-        pass
+        mock_func.return_value = False
+        expected = ['test_package1', 'test_package2']
+        result = pip2.commands.install.install(expected)
 
+        assert result['installed'] == []
+        assert result['failed'] == expected
+
+
+@mock.patch.object(distutils2.install, 'install')
 class TestInstallCLI():
     
-    @mock.patch.object(pip2.commands.install, 'func')
     def test_install_single_package(self, mock_func):
         pass
 
-    @mock.patch.object(pip2.commands.install, 'func')
     def test_install_multiple_packages(self, mock_func):
         pass
 
-    @mock.patch.object(pip2.commands.install, 'func')
     def test_install_fail_single_package(self, mock_func):
         pass
 
-    @mock.patch.object(pip2.commands.install, 'func')
     def test_install_fail_multiple_packages(self, mock_func):
         pass
 
