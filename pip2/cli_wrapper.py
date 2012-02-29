ae6b929c695e63f0b120138b7d793601c649d5a6
michaeta <ot41is@gmail.com>
2012-02-28 20:40:06 -0800

    search displays properly now, but does not get terminal width from system yet. Added some try/except around inner search API calls for future reference.

--- pip2/cli_wrapper.py
+++ pip2/cli_wrapper.py
@@ -1,7 +1,7 @@
 import pip2.commands.install
 import pip2.commands.freeze
 import pip2.commands.search
-import sys
+import sys, os
 
 def install(args):
     result = pip2.commands.install.install(args.package_list)
@@ -25,16 +25,32 @@ def freeze(args):
 
 def search(args):
     results, matches = pip2.commands.search.search(args.package)
+    terminal_width = 80 #get actual terminal width somehow?
+    name_len = 26 #package name alotted this many characters
+    # package summary alotted this many characters per line, - 4 is for the ' - ' and 
+    # leaving one extra space at end
+    sum_len = terminal_width - name_len - 4 # 50
     
     #for each search result
     for res_key in results.keys():
-        #print the package name and summary
-        print("{0} - {1}".format(res_key, results[res_key]))
+        # how much of the summary we have printed
+        printed = 0
+        #get as much as can fit on the first line
+        summary = results[res_key][:sum_len]
+        result = "{0:<{2}} - {1}".format(res_key[:name_len], summary, name_len)
+        print(result)
+        printed += sum_len
+        # while we haven't printed all of the summary
+        while(printed) < (len(results[res_key])):
+            # +3 is to compensate for ' - '
+            result = " "*(name_len+3) + results[res_key][printed:(printed+sum_len)]
+            print(result)
+            printed += sum_len
         #if the user already has the package installed print their installed version
         #and the latest version found on the index
         if res_key.lower() in matches.keys():
-            print("INSTALLED: {0}\nLATEST   : {1}".format(matches[res_key.lower()]['installed'], 
-                                                          matches[res_key.lower()]['latest']))
+            print("   INSTALLED: {0}\n   LATEST   : {1}".format(matches[res_key.lower()]['installed'], 
+                                                                matches[res_key.lower()]['latest']))
     return
     
     
--- pip2/commands/search.py
+++ pip2/commands/search.py
@@ -9,20 +9,32 @@ def search(package):
 
     #dictionary to hold results. Key is package name, value is summary of package
     results = dict()
+    #dictionary to hold matches of search results and installed packages
+    #key is package name and value is dictionary with installed version and queried version
     matches = dict()
 
     #create the client to connect to package indexes
-    client = xmlrpc.Client()
+    try: #temp for future ref, fail gracefully
+        client = xmlrpc.Client()
+    except:
+        raise
     
     #get installed packages for comparisons against search results (will also need version numbers eventually)
     installed = [dis.lower() for dis in pip2.commands.freeze.freeze()]
     #search_projects returns a list of projects
-    projects = client.search_projects(name = package)
+    try: #temp for future ref
+        projects = client.search_projects(name = package)
+    except:
+        raise
     
     #for each project, which is really just a releasesList 
     for project in projects:
         #gets the latest release
-        release = project.releases[0]
+        try: #temp for future ref. Sometimes a release can't be found or version number can't be rationalized
+            release = project.releases[0]
+        except:
+            raise
+        
         results[project.name] = release.metadata['summary']
         #if this package from search results is already installed then keep track of it
         if project.name.lower() in installed:
