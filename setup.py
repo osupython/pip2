1af33558daffdd7a25db4a0b2aab52e0e2a04ae5
Nick Wilson <nick@njwilson.net>
2012-05-02 16:19:35 -0700

    Clean up setup.py
    - Use README.rst for the long description
    
    - Update the list of supported Python versions
    
    - Remove the test stuff (it wasn't working/up-to-date; we'll implement
      it correctly later if it turns out we need it)
    
    - Minor cleanup

--- setup.py
+++ setup.py
@@ -1,36 +1,39 @@
 import os
 import sys
+
 from setuptools import setup
 
 
 version = "0.0.1"
 
-long_description = """
+install_requires = []
 
-Experimental port of pip to use the distutils2 packaging library.
+if sys.version_info < (3, 3):
+    # Distutils2 on PyPI doesn't work for Python 3, so we aren't listing it as
+    # a dependency yet (see Github issue 45)
+    #install_requires.append('distutils2')
+    pass
 
-"""
 
 setup(name="pip2",
       version=version,
       description="Experimental port of pip to use distutils2.",
-      long_description=long_description,
+      long_description=open('README.rst').read(),
       classifiers=[
           'Development Status :: 2 - Pre-Alpha',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Topic :: Software Development :: Build Tools',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.1',
           'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
       ],
-      keywords='easy_install distutils2 setuptools egg virtualenv',
+      keywords='pip packaging distutils2 easy_install setuptools',
       author='OSUPython Senior Design Team',
       author_email='osupython@gmail.com',
-      url='http://https://github.com/osupython/pip2',
+      url='http://github.com/osupython/pip2',
       license='MIT',
       packages=['pip2', 'pip2.commands'],
       entry_points=dict(console_scripts=['pip2=pip2:main']),
-      test_suite='nose.collector',
-      tests_require=['nose', 'virtualenv>=1.7', 'scripttest>=1.1.1', 'mock'],
+      install_requires=install_requires,
       zip_safe=False)
