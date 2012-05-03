b9e694fc26bbd36f36fe48f105da3cc4350aa6e6
Nick Wilson <nick@njwilson.net>
2012-05-03 16:36:05 -0700

    Rename mock_func to mock_install

--- tests/test_install.py
+++ tests/test_install.py
@@ -8,32 +8,32 @@ import tempfile
 @mock.patch.object(packaging.install, 'install')
 class TestInstallAPI():
 
-    def test_install_single_package(self, mock_func):
-        mock_func.return_value = True
+    def test_install_single_package(self, mock_install):
+        mock_install.return_value = True
         expected = ['test_package1']
         result = pip2.commands.install.install(expected)
 
         assert result['installed'] == expected
         assert result['failed'] == []
 
-    def test_install_multiple_packages(self, mock_func):
-        mock_func.return_value = True
+    def test_install_multiple_packages(self, mock_install):
+        mock_install.return_value = True
         expected = ['test_package1', 'test_package2', 'test_package3']
         result = pip2.commands.install.install(expected)
 
         assert result['installed'] == expected
         assert result['failed'] == []
 
-    def test_install_fail_single_package(self, mock_func):
-        mock_func.return_value = False
+    def test_install_fail_single_package(self, mock_install):
+        mock_install.return_value = False
         expected = ['failing_package']
         result = pip2.commands.install.install(expected)
 
         assert result['installed'] == []
         assert result['failed'] == expected
 
-    def test_install_fail_multiple_packages(self, mock_func):
-        mock_func.return_value = False
+    def test_install_fail_multiple_packages(self, mock_install):
+        mock_install.return_value = False
         expected = ['test_package1', 'test_package2']
         result = pip2.commands.install.install(expected)
 
@@ -55,19 +55,17 @@ class TestInstallAPI():
         assert mock_install.called == True
 
 
-
-
 @mock.patch.object(packaging.install, 'install')
 class TestInstallCLI():
 
     def test_install_single_package(self, mock_func):
         pass
 
-    def test_install_multiple_packages(self, mock_func):
+    def test_install_multiple_packages(self, mock_install):
         pass
 
-    def test_install_fail_single_package(self, mock_func):
+    def test_install_fail_single_package(self, mock_install):
         pass
 
-    def test_install_fail_multiple_packages(self, mock_func):
+    def test_install_fail_multiple_packages(self, mock_install):
         pass
