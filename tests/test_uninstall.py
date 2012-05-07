1d50096f58449c184474d3191e9b5b1d16f741bf
Taylor Hand <thand256@gmail.com>
2012-05-07 16:08:52 -0700

    Updated uninstall's tests to mirror install's.

--- tests/test_uninstall.py
+++ tests/test_uninstall.py
@@ -1,2 +1,59 @@
+"""
+Mock testing for uninstall command.
+"""
+
 import mock
 import pip2.commands.uninstall
+import packaging.install
+
+
+@mock.patch.object(packaging.install, 'remove')
+class TestUninstallAPI():
+
+    def test_uninstall_single_package(self, mock_func):
+        mock_func.return_value = True
+        expected = ['test_package1']
+        result = pip2.commands.uninstall.uninstall(expected)
+
+        assert result['uninstalled'] == expected
+        assert result['failed'] == []
+
+    def test_uninstall_multiple_packages(self, mock_func):
+        mock_func.return_value = True
+        expected = ['test_package1', 'test_package2', 'test_package3']
+        result = pip2.commands.uninstall.uninstall(expected)
+
+        assert result['uninstalled'] == expected
+        assert result['failed'] == []
+
+    def test_uninstall_fail_single_package(self, mock_func):
+        mock_func.return_value = False
+        expected = ['failing_package1']
+        result = pip2.commands.uninstall.uninstall(expected)
+
+        assert result['uninstalled'] == []
+        assert result['failed'] == expected
+
+    def test_uninstall_fail_multiple_packages(self, mock_func):
+        mock_func.return_value = False
+        expected = ['failing_package1', 'failing_package2']
+        result = pip2.commands.uninstall.uninstall(expected)
+
+        assert result['uninstalled'] == []
+        assert result['failed'] == expected
+
+
+@mock.patch.object(packaging.install, 'remove')
+class TestUninstallCLI():
+
+    def test_uninstall_single_package(self, mock_func):
+        pass
+
+    def test_uninstall_multiple_packages(self, mock_func):
+        pass
+
+    def test_uninstall_fail_single_package(self, mock_func):
+        pass
+
+    def test_uninstall_fail_multiple_packages(self, mock_func):
+        pass
