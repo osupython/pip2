a6d2ffd77f3c556f16118226c630c642259ee3fb
Ryan Baker <dev@rlbaker.net>
2012-05-10 14:03:48 -0700

    Added help messages to each command

--- pip2/pip_parser.py
+++ pip2/pip_parser.py
@@ -11,18 +11,18 @@ def create_parser():
     parser = argparse.ArgumentParser(prog='pip2')
     subparsers = parser.add_subparsers()
 
-    parser_install = subparsers.add_parser('install')
-    parser_install.add_argument('package_list', nargs='+')
+    parser_install = subparsers.add_parser('install', help='- Install a package or list of packages')
+    parser_install.add_argument('package_list', nargs='+', help='- Package or list of packages to install')
     parser_install.set_defaults(func=pip2.cli_wrapper.install)
 
-    parser_uninstall = subparsers.add_parser('uninstall')
-    parser_uninstall.add_argument('package_list', nargs='+')
+    parser_uninstall = subparsers.add_parser('uninstall', help='- Uninstall a package or list of packages')
+    parser_uninstall.add_argument('package_list', nargs='+', help='- Package or list of packages to uninstall')
     parser_uninstall.set_defaults(func=pip2.cli_wrapper.uninstall)
 
-    parser_freeze = subparsers.add_parser('freeze')
+    parser_freeze = subparsers.add_parser('freeze', help='- Display locally installed packages')
     parser_freeze.set_defaults(func=pip2.cli_wrapper.freeze)
 
-    parser_search = subparsers.add_parser('search')
+    parser_search = subparsers.add_parser('search', help='- Search for a package on PyPi')
     parser_search.add_argument('package')
     parser_search.set_defaults(func=pip2.cli_wrapper.search)
 
